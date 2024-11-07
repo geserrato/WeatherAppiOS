e8614a07febd0a203d6b691dfc82fa2ccd7f6622
Gerardo Estrella <g.serrato41@outlook.com>
2024-11-06 21:01:11 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Gerardo Estrella on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
